ad0667191420ca750f2af2bd1153d23dcc3c6c52
Taehyun Lim <admin@limtaehyun.dev>
2022-09-20 21:32:26 +0900

    feat: create action

---
+++ actions/approve/index.js
@@ -0,0 +1,12 @@
+// const AWS = require('aws-sdk')
+// const dotenv = reuqire('dotenv')
+
+const json = process.argv[2]
+const id = process.argv[3]
+
+console.log(json, id)
+
+// dotenv.config()
+// AWS.config.update({ accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, region: process.env.AWS_REGION })
+
+// const ec2 = new AWS.EC2({ region: 'ap-northeast-2' })
